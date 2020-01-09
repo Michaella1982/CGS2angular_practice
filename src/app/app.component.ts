ffd3e91283862cf9007441a33b74175e62572d42
Michaella Babbitt <tinydynamine@gmail.com>
2020-01-08 20:14:15 -0600

    first commit!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from './movie';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies';
+
+  watched = [
+    new Movie("Jumanji", "comedy", 2019),
+    new Movie("Rise of Skywalker","action", 2019),
+    new Movie("IT: Chapter II", "horror", 2019)
+  ]
+  toWatch = [
+    new Movie("Grudge", "horror", 2020),
+    new Movie("Turning","horror", 2020),
+    new Movie("Parasite", "comedy/thriller", 2019),
+    new Movie("United Skates", "documentary", 2019)
+
+  ]
+  
 }
+
+
---
+++ src/app/movies-list/movies-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../movie';
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrls: ['./movies-list.component.css']
+})
+export class MoviesListComponent implements OnInit {
+
+  @Input() movies:Movie[];
+  @Input() name: String;
+
+
+  constructor() { 
+    
+
+  }
+
+  ngOnInit() {
+  }
+
+  details(movie: Movie){
+    alert(`details for ${movie.title}`);
+  }
+
+}
